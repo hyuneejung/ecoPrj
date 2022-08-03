df0ccc2ea6f1a26bee6a6857b5532b48894581e4
TAEWON\xodnj <xodnjs2619@naver.com>
2022-08-03 14:27:40 +0900

    태원 커밋~
    포인트 랭크 까페DTO

---
+++ ecoMiniPrj/src/rankUpdate/RankUpdateController.java
@@ -0,0 +1,23 @@
+package rankUpdate;
+
+import main.Main;
+import member.EcoDto;
+
+public class RankUpdateController {
+	
+	public void rankUpdate() {
+		if(Main.LoginUser == null) {
+			return;
+		}
+		EcoDto ed = Main.LoginUser;
+		/*
+		 * 업데이트전 랭크받아오기
+		 * 
+		 * 업데이트후 등급업 되면 등급업 축하메시지 로직
+		 * 
+		 * 
+		 * */
+		new RankUpdateService().rankUpdate(ed);
+		
+	}
+}
