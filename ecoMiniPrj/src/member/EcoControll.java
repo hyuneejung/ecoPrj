8cf3f5e9c7462a9c16beae5bfc7d5e308c5ed3ff
DESKTOP-SVVL3BD\User <kskoo94@naver.com>
2022-08-04 16:01:44 +0900

    구기석 커밋요청
    로그인, dto
    SQL에 커밋하신후에하면 실행되요!

--- ecoMiniPrj/src/member/EcoControll.java
+++ ecoMiniPrj/src/member/EcoControll.java
@@ -13,275 +13,282 @@ import util.MiniConn;
 
 public class EcoControll {
 
-   public EcoDto login(EcoDto data) {
-      String id = data.getId();
-      String pwd = data.getPwd();
+	public EcoDto login(EcoDto data) {
+		String id = data.getId();
+		String pwd = data.getPwd();
 
-      // 드라이버 등록
-      // 드라이버 이용해서 연결 가져오기
-      Connection conn = MiniConn.getConnection();
+		// 드라이버 등록
+		// 드라이버 이용해서 연결 가져오기
 
-      PreparedStatement pstmt = null;
-      ResultSet rs = null;
+		Connection conn = null;
+//		Connection conn = MiniConn.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-      // SQL 실행 --?
-      String sql = "SELECT E.NO, E.ID, E.NAME, E.NICK, E.GENDER, E.PHONE, B.NAME , E.ACCOUNT, E.POINT, E.ADDEDPOINT, E.ENROLL_DATE, R.RANK\r\n"
-      		+ "FROM ECO_MEMBER E\r\n"
-      		+ "JOIN BANKCODE B ON E.BANKNO = B.BANKNO\r\n"
-      		+ "JOIN BANKSYS R ON E.RANKNO = R.NO\r\n"
-      		+ "WHERE E.QUIT_YN = 'N' AND ID = ? AND PWD = ?\r\n";
-      		
-      EcoDto loginEco = null;
+		// SQL 실행 --?
+		String sql = "SELECT E.NO, E.ID, NAME, E.NICK, E.GENDER, E.PHONE, B.BANKNAME , E.ACCOUNT, E.POINT, E.ADDEDPOINT, E.ENROLL_DATE, R.RANK\r\n"
+				+ "FROM ECO_MEMBER E\r\n"
+				+ "JOIN BANKCODE B ON E.BANKNO = B.NO\r\n"
+				+ "JOIN RANKSYS R ON E.RANKNO = R.NO\r\n"
+				+ "WHERE E.QUIT_YN = 'N' AND ID = ? AND PWD = ?";
+		
+				
+//		String sql = "SELECT * FROM ECO_MEMBER WHERE ID = ? AND PWD = ? AND QUIT_YN = 'N'" ;
+		
+		EcoDto loginEco = null;
 
-      try {
-         pstmt = conn.prepareStatement(sql);
-         //
-         pstmt.setString(1, id);
-         pstmt.setString(2, pwd);
-         // 지금 실행하는 쿼리는 ? SELECT 쿼리 -> 결과집합 (ResultSet)
-         rs = pstmt.executeQuery();
-         //
-         if (rs.next()) {
-        	 String ecoid = rs.getString("ID");
-        	   String name = rs.getString("NAME");
-        	   String nick = rs.getString("NICK");
-        	   String gender = rs.getString("GENDER");
-        	   String phone = rs.getString("PHONE");
-        	   String bankNo = rs.getString("NAME_1");
-        	   String account = rs.getString("ACCOUNT");
-        	   int point = rs.getInt("POINT");
-        	   int addedPoint = rs.getInt("ADDEDPOINT");
-        	   Timestamp enrolldate = rs.getTimestamp("ENROLL_DATE");
-        	   String rank = rs.getString("RANK");
-        	   String quit_yn = rs.getString("QUIT_YN");
-            loginEco = new EcoDto();
+		try {
+			conn = MiniConn.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			//
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			// 지금 실행하는 쿼리는 ? SELECT 쿼리 -> 결과집합 (ResultSet)
+			rs = pstmt.executeQuery();
+			//
+			if (rs.next()) {
+				String ecoid = rs.getString("ID");
+				String name = rs.getString("NAME");
+				String nick = rs.getString("NICK");
+				String gender = rs.getString("GENDER");
+				String phone = rs.getString("PHONE");
+				String bankName = rs.getString("BANKNAME");
+				String account = rs.getString("ACCOUNT");
+				int point = rs.getInt("POINT");
+				int addedPoint = rs.getInt("ADDEDPOINT");
+				Timestamp enrolldate = rs.getTimestamp("ENROLL_DATE");
+				String rankNo = rs.getString("RANK");
 
-            loginEco.setId(ecoid);
-            loginEco.setPwd(pwd);
-            loginEco.setName(name);
-            loginEco.setNick(nick);
-            loginEco.setGender(gender);
-            loginEco.setPhone(phone);
-            loginEco.setBankNo(bankNo);
-            loginEco.setAccount(account);
-            loginEco.setPoint(point);
-            loginEco.setAddedPoint(addedPoint);
-            loginEco.setRank(rank);
-            loginEco.setRank(rank);
-            loginEco.setEnroll_date(enrolldate);
+				loginEco = new EcoDto();
 
-         }
-      } catch (SQLException e) {
-         System.out.println("로그인 오류입니다.");
-         e.printStackTrace();
-      } finally {
-         MiniConn.close(conn);
-         MiniConn.close(pstmt);
-         MiniConn.close(rs);
-      }
-      // 로그인처리
-      Main.LoginUser = loginEco;
-      return loginEco;
-   }
+				loginEco.setId(ecoid);
+				loginEco.setPwd(pwd);
+				loginEco.setName(name);
+				loginEco.setNick(nick);
+				loginEco.setGender(gender);
+				loginEco.setPhone(phone);
+				loginEco.setBankName(bankName); 
+				loginEco.setAccount(account);
+				loginEco.setPoint(point);
+				loginEco.setAddedPoint(addedPoint);
+				loginEco.setRankName(rankNo);
+				loginEco.setEnroll_date(enrolldate);
 
-   public void login() {
-      Scanner sc = new Scanner(System.in);
-      System.out.println("아이디를 입력해주세요  : ");
-      String id = sc.nextLine();
-      System.out.println("비밀번호를 입력해주세요 : ");
-      String pwd = sc.nextLine();
+			}
+		} catch (SQLException e) {
+			System.out.println("로그인 오류입니다.");
+			e.printStackTrace();
+		} finally {
+			MiniConn.close(conn);
+			MiniConn.close(pstmt);
+			MiniConn.close(rs);
+		}
+		// 로그인처리
+		Main.LoginUser = loginEco;
+//		return loginEco;
+		return loginEco;
+	}
 
-      EcoDto dto = new EcoDto();
-      dto.setId(id);
-      dto.setPwd(pwd);
 
-      // 로그인 처리
-      login(dto);
+	public void login() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("아이디를 입력해주세요  : ");
+		String id = sc.nextLine();
+		System.out.println("비밀번호를 입력해주세요 : ");
+		String pwd = sc.nextLine();
+		EcoDto dto = new EcoDto();
+		dto.setId(id);
+		dto.setPwd(pwd);
 
-      if (Main.LoginUser != null) {
-         System.out.println("로그인에 성공하셨습니다.");
-      } else {
-         System.out.println("--로그인 실패--");
-         System.out.println("아이디 / 비밀번호 를 다시 입력해주세요. ");
-      }
-   }// 로그인
+		// 로그인 처리
+		login(dto);
 
-   public void loginCheck(EcoDto dto) {
-      if (dto != null) {
-         System.out.println("<====사용자 정보====>");
-         System.out.println(dto);
-      } else {
-         System.out.println("로그인실패");
-      }
+		if (Main.LoginUser != null) {
+			System.out.println("로그인에 성공하셨습니다.");
+		} else {
+			System.out.println("--로그인 실패--");
+			System.out.println("아이디 / 비밀번호 를 다시 입력해주세요. ");
+		}
+	}// 로그인
 
-   }// 로긴 체크
+	public void loginCheck(EcoDto dto) {
+		if (dto != null) {
+			System.out.println("<====사용자 정보====>");
+			System.out.println(dto);
+		} else {
+			System.out.println("로그인실패");
+		}
 
-   public String findId(String name, String phone) {
+	}// 로긴 체크
 
-      Connection conn = MiniConn.getConnection();
+	public String findId(String name, String phone) {
 
-      PreparedStatement pstmt = null;
-      ResultSet rs = null;
+		Connection conn = MiniConn.getConnection();
 
-      String sql = "SELECT ID FROM ECO_MEMBER WHERE NAME = ? AND PHONE = ?";
-      String getId = null;
-      try {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
-         pstmt = conn.prepareStatement(sql);
-         pstmt.setString(1, name);
-         pstmt.setString(2, phone);
-         rs = pstmt.executeQuery();
+		String sql = "SELECT ID FROM ECO_MEMBER WHERE NAME = ? AND PHONE = ?";
+		String getId = null;
+		try {
 
-         rs.next();
-         getId = rs.getString("ID");
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, phone);
+			rs = pstmt.executeQuery();
 
-      } catch (SQLException e) {
-         System.out.println("ID 찾기 실패 에러");
-         e.printStackTrace();
-      } finally {
-         MiniConn.close(rs);
-         MiniConn.close(pstmt);
-         MiniConn.close(conn);
-      }
-      return getId;
-   }
-   public void findUser() {
+			rs.next();
+			getId = rs.getString("ID");
 
-      System.out.print("이름 : ");
-      String name = InputUtil.getString();
-      System.out.print("전화번호 : ");
-      String phone = InputUtil.getString();
+		} catch (SQLException e) {
+			System.out.println("ID 찾기 실패 에러");
+			e.printStackTrace();
+		} finally {
+			MiniConn.close(rs);
+			MiniConn.close(pstmt);
+			MiniConn.close(conn);
+		}
+		return getId;
+	}
 
-      String rdn = InputUtil.randomNum(6, 1);
-      System.out.println(" 인증번호 6자리 ::" + rdn);
-      System.out.print("입력번호 ::::");
+	public void findUser() {
 
-      String rdnCheck = InputUtil.getString();
+		System.out.print("이름 : ");
+		String name = InputUtil.getString();
+		System.out.print("전화번호 : ");
+		String phone = InputUtil.getString();
 
-      if (rdn.equals(rdnCheck)) {
-         String getId = findId(name, phone);
-         System.out.println("아이디를 찾았습니다 ! : " + getId);
+		String rdn = InputUtil.randomNum(6, 1);
+		System.out.println(" 인증번호 6자리 ::" + rdn);
+		System.out.print("입력번호 ::::");
 
-      } else {
-         System.out.println("인증번호가 일치하지않습니다.");
-      }
-   }
+		String rdnCheck = InputUtil.getString();
 
-   public void changePwd() {
-      System.out.print("이름을 입력해주세요 ::");
-      String name = InputUtil.getString();
-      System.out.print("아이디를 입력해주세요 ::");
-      String id = InputUtil.getString();
-      System.out.print("전화번호를 입력해주세요::");
-      String phone = InputUtil.getString();
+		if (rdn.equals(rdnCheck)) {
+			String getId = findId(name, phone);
+			System.out.println("아이디를 찾았습니다 ! : " + getId);
 
-      String rdn = InputUtil.randomNum(6, 1);
-      System.out.println("인증번호 6자리를 입력하세요 ::::" + rdn);
-      System.out.print("입력 인증번호 ::::");
-      String rdnCheck = InputUtil.getString();
-      if (rdnCheck.equals(rdn)) { // 인증번호 일치
-         // 기존비밀번호가 맞는지 체크
-         EcoDto dto = findInDb(name, id, phone);
-         if(dto == null) {   
-            System.out.println("입력한 사용자가 존재하지않습니다.");
-            return;
-         }
-         String pwdCheck = dto.getPwd();
+		} else {
+			System.out.println("인증번호가 일치하지않습니다.");
+		}
+	}
 
-         System.out.print("새로운 비밀번호를 입력해주세요 ::");
-         String chPwd = InputUtil.getString();
-         System.out.print("비밀번호를 한번더 입력해주세요");
-         String chPwd2 = InputUtil.getString();
+	public void changePwd() {
+		System.out.print("이름을 입력해주세요 ::");
+		String name = InputUtil.getString();
+		System.out.print("아이디를 입력해주세요 ::");
+		String id = InputUtil.getString();
+		System.out.print("전화번호를 입력해주세요::");
+		String phone = InputUtil.getString();
 
-         if (chPwd.equals(chPwd2)) {
+		String rdn = InputUtil.randomNum(6, 1);
+		System.out.println("인증번호 6자리를 입력하세요 ::::" + rdn);
+		System.out.print("입력 인증번호 ::::");
+		String rdnCheck = InputUtil.getString();
+		if (rdnCheck.equals(rdn)) { // 인증번호 일치
+			// 기존비밀번호가 맞는지 체크
+			EcoDto dto = findInDb(name, id, phone);
+			if (dto == null) {
+				System.out.println("입력한 사용자가 존재하지않습니다.");
+				return;
+			}
+			String pwdCheck = dto.getPwd();
 
-            if (chPwd.equals(pwdCheck)) {
-               System.out.println("기존 비밀번호와 신규 비밀번호가 동일합니다.");
-               // 비밀번호 일치
-               
-            }else {
-            int r =   updatePwd(dto,chPwd);
-            if(r == 1) {
-               System.out.println("비밀번호 변경이 완료되었습니다.");
-            }else {
-               System.out.println("비밀번호 변경 실패");
-            }
-            }
-         } else {
-            System.out.println("입력하신 비밀번호가 서로 일치하지않습니다.");
-         }
-      } // 인증번호 IF
-      else {
-         System.out.println("인증번호가 일치하지않습니다.");
-      } // 인증번호 입력값이다를떄
-   }// 비번변경
+			System.out.print("새로운 비밀번호를 입력해주세요 ::");
+			String chPwd = InputUtil.getString();
+			System.out.print("비밀번호를 한번더 입력해주세요");
+			String chPwd2 = InputUtil.getString();
 
-   private EcoDto findInDb(String name, String id, String phone) {
-      // 기존 비밀번호(db)를 받아와야함
-      // 데이터 베이스에있는 아이디의 비밀번호 작성
-      EcoDto dto = null;
-      Connection conn = MiniConn.getConnection();
-      PreparedStatement pstmt = null;
+			if (chPwd.equals(chPwd2)) {
 
-      String sql = "SELECT * FROM ECO_MEMBER WHERE ID = ? AND NAME = ? AND PHONE = ?";
+				if (chPwd.equals(pwdCheck)) {
+					System.out.println("기존 비밀번호와 신규 비밀번호가 동일합니다.");
+					// 비밀번호 일치
 
-      ResultSet rs = null;
-      try {
-         pstmt = conn.prepareStatement(sql);
-         pstmt.setString(1, id);
-         pstmt.setString(2, name);
-         pstmt.setString(3, phone);
+				} else {
+					int r = updatePwd(dto, chPwd);
+					if (r == 1) {
+						System.out.println("비밀번호 변경이 완료되었습니다.");
+					} else {
+						System.out.println("비밀번호 변경 실패");
+					}
+				}
+			} else {
+				System.out.println("입력하신 비밀번호가 서로 일치하지않습니다.");
+			}
+		} // 인증번호 IF
+		else {
+			System.out.println("인증번호가 일치하지않습니다.");
+		} // 인증번호 입력값이다를떄
+	}// 비번변경
 
-         rs = pstmt.executeQuery();
-         while (rs.next()) {
-            dto = new EcoDto();
-            dto.setId(rs.getString("ID"));
-            dto.setName(rs.getString("NAME"));
-            dto.setPhone(rs.getString("PHONE"));
-            return dto;
-         }
+	private EcoDto findInDb(String name, String id, String phone) {
+		// 기존 비밀번호(db)를 받아와야함
+		// 데이터 베이스에있는 아이디의 비밀번호 작성
+		EcoDto dto = null;
+		Connection conn = MiniConn.getConnection();
+		PreparedStatement pstmt = null;
 
-      } catch (SQLException e) {
-         MiniConn.rollback(conn);
+		String sql = "SELECT * FROM ECO_MEMBER WHERE ID = ? AND NAME = ? AND PHONE = ?";
 
-         e.printStackTrace();
-      } finally {
-         MiniConn.close(conn);
-         MiniConn.close(pstmt);
+		ResultSet rs = null;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, name);
+			pstmt.setString(3, phone);
 
-      }
-      return dto;
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				dto = new EcoDto();
+				dto.setId(rs.getString("ID"));
+				dto.setName(rs.getString("NAME"));
+				dto.setPhone(rs.getString("PHONE"));
+				return dto;
+			}
 
-   }
-   private int updatePwd(EcoDto dto, String newPwd) {
+		} catch (SQLException e) {
+			MiniConn.rollback(conn);
 
-      Connection conn = MiniConn.getConnection();
-      String sql = "UPDATE ECO_MEMBER SET PWD = ? WHERE ID = ?";
+			e.printStackTrace();
+		} finally {
+			MiniConn.close(conn);
+			MiniConn.close(pstmt);
 
-      PreparedStatement pstmt = null;
-      int result = 0;
-      try {
-         pstmt = conn.prepareStatement(sql);
-         pstmt.setString(1, newPwd);
-         pstmt.setString(2, dto.getId()); // 여기에 dto.getId가 들어가야함.
+		}
+		return dto;
 
-         result = pstmt.executeUpdate();
+	}
 
-         if (result == 1) {
-            conn.commit();
-         } else {
-            conn.rollback();
-         }
+	private int updatePwd(EcoDto dto, String newPwd) {
 
-      } catch (SQLException e) {
-         MiniConn.rollback(conn);
+		Connection conn = MiniConn.getConnection();
+		String sql = "UPDATE ECO_MEMBER SET PWD = ? WHERE ID = ?";
 
-         e.printStackTrace();
-      } finally {
-         MiniConn.close(conn);
-         MiniConn.close(pstmt); 
-      }
-      return result;
-   }
+		PreparedStatement pstmt = null;
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, newPwd);
+			pstmt.setString(2, dto.getId()); // 여기에 dto.getId가 들어가야함.
+
+			result = pstmt.executeUpdate();
+
+			if (result == 1) {
+				conn.commit();
+			} else {
+				conn.rollback();
+			}
+
+		} catch (SQLException e) {
+			MiniConn.rollback(conn);
+
+			e.printStackTrace();
+		} finally {
+			MiniConn.close(conn);
+			MiniConn.close(pstmt);
+		}
+		return result;
+	}
 
 }
